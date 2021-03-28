62f5e622dfa7a114b6f046baacfb5e97b7ed4a3d
Justice Almanzar <superdash993@gmail.com>
2021-03-28 13:01:01 -0400

    Update packages from 6018f5a :rocket:

---
+++ packages/vue-heroicons/components/ArrowCircleRightSolid.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ArrowCircleRightSolid',
+  functional: true,
+  props: {
+    size: { type: Number, default: 0 },
+  },
+  render (createElement, context) {
+    const data = { ...context.data }
+    if (context.props.size) {
+      data.attrs.width = context.props.size
+      data.attrs.height = context.props.size
+    }
+    data.attr = {
+      ...data.attr,
+      viewBox: `0 0 20 20`,
+      xmlns: 'http://www.w3.org/2000/svg',
+      fill: 'currentColor',
+      stroke: 'none',
+    }
+    data.domProps.innerHTML = '$SVGINNER'
+    return createElement('svg', data)
+  },
+})
---
+++ packages/vue-heroicons/components/ChatAlt2.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ChatAlt2',
+  functional: true,
+  props: {
+    size: { type: Number, default: 0 },
+  },
+  render (createElement, context) {
+    const data = { ...context.data }
+    if (context.props.size) {
+      data.attrs.width = context.props.size
+      data.attrs.height = context.props.size
+    }
+    data.attr = {
+      ...data.attr,
+      viewBox: `0 0 24 24`,
+      xmlns: 'http://www.w3.org/2000/svg',
+      fill: 'none',
+      stroke: 'currentColor',
+    }
+    data.domProps.innerHTML = '$SVGINNER'
+    return createElement('svg', data)
+  },
+})
